1e02e24be13c0ec3b408d51fd3d6cbe0e3f0c338
Taras Novak <taras.novak@randomfractals.com>
2016-12-04 03:28:11 -0600

    added extra checks for proper token/secret/keys config

--- tests/config.test.js
+++ tests/config.test.js
@@ -16,17 +16,20 @@ describe('App Config test', () => {
   it('WIT_TOKEN set test', () => {
     const config = require('../config.js');
     expect(config.WIT_TOKEN).not.toBe(null);
+    expect(config.WIT_TOKEN).not.toContain('token>');
   });
 
   it('FB_PAGE_TOKEN set test', () => {
     const config = require('../config.js');
     expect(config.FB_PAGE_TOKEN).not.toBe(null);
+    expect(config.WIT_TOKEN).not.toContain('token>');    
   });
 
 
   it('FB_APP_SECRET set test', () => {
     const config = require('../config.js');
     expect(config.FB_APP_SECRET).not.toBe(null);
+    expect(config.WIT_TOKEN).not.toContain('secret>');    
   });
 
   it('FB_VERIFY_TOKEN set test', () => {
@@ -37,6 +40,7 @@ describe('App Config test', () => {
   it('CENSUS_DATA_API_KEY set test', () => {
     const config = require('../config.js');
     expect(config.CENSUS_DATA_API_KEY).not.toBe(null);
+    expect(config.WIT_TOKEN).not.toContain('key>');    
   });
   
 });
