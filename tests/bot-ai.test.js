3a793c5df1b594735621d4d3f564973d837aea71
Taras Novak <taras.novak@randomfractals.com>
2016-12-08 09:23:36 -0600

    added starter bot-ai.test.js

---
+++ tests/bot-ai.test.js
@@ -0,0 +1,32 @@
+jest.dontMock('../bot-ai.js');
+
+const botAI = require('../bot-ai.js');
+
+describe('Bot AI Interface Tests', () => {
+
+  it('processMessage() is defined', () => {
+    expect(botAI.processMessage).toBeDefined();
+  });
+});
+
+describe('Bot AI processMessage() Tests', () => {
+
+  it('fails on missing message text', () => {
+    expect( () => {
+      botAI.processMessage({
+        sender: {}, recipient: {}, message: {}
+      });
+    }).toThrowError('Missing message text.');
+  });
+
+  it('What is the population of USA?', () => {
+    return expect( () => {
+      botAI.processMessage({
+        sender: {id: "1165704360144557"}, 
+        recipient: {id: "PAGE_ID"}, 
+        message: {text: "What is the population of USA?"}
+      });
+    }).toBeDefined(); // TODO: should return current USA pop text response
+  });    
+
+});
