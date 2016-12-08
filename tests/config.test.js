50a3b80ef0f9c48b99f8080e4715c5c764f57912
Taras Novak <taras.novak@randomfractals.com>
2016-12-08 04:53:31 -0600

    config.test.js messages changes

--- tests/config.test.js
+++ tests/config.test.js
@@ -13,31 +13,31 @@ describe('App Config Tests', () => {
     expect(config.WIT_TOKEN).toEqual('wit_token');
   });
 
-  it('WIT_TOKEN set test', () => {
+  it('WIT_TOKEN is set', () => {
     const config = require('../config.js');
     expect(config.WIT_TOKEN).not.toBe(null);
     expect(config.WIT_TOKEN).not.toContain('token>');
   });
 
-  it('FB_PAGE_TOKEN set test', () => {
+  it('FB_PAGE_TOKEN is set', () => {
     const config = require('../config.js');
     expect(config.FB_PAGE_TOKEN).not.toBe(null);
     expect(config.WIT_TOKEN).not.toContain('token>');    
   });
 
 
-  it('FB_APP_SECRET set test', () => {
+  it('FB_APP_SECRET is set', () => {
     const config = require('../config.js');
     expect(config.FB_APP_SECRET).not.toBe(null);
     expect(config.WIT_TOKEN).not.toContain('secret>');    
   });
 
-  it('FB_VERIFY_TOKEN set test', () => {
+  it('FB_VERIFY_TOKEN is created', () => {
     const config = require('../config.js');
     expect(config.FB_VERIFY_TOKEN).not.toBe(null);
   });
 
-  it('CENSUS_DATA_API_KEY set test', () => {
+  it('CENSUS_DATA_API_KEY is set', () => {
     const config = require('../config.js');
     expect(config.CENSUS_DATA_API_KEY).not.toBe(null);
     expect(config.WIT_TOKEN).not.toContain('key>');    
