a0612f88eb297f8ea495cab7c7f8fd771e4901e2
Taras Novak <taras.novak@randomfractals.com>
2016-12-12 18:50:24 -0600

    added Census js data service stub

---
+++ src/census/census.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**
+ * Defines top-level Census data service api for getting US pop,
+ * biz, trade, incomes, and housing data stats.  
+ */
+class Census {
+
+  /**
+  * Creates new Census data service instance.
+  *
+  * @param config Census data service config.
+  */
+  constructor(config) {
+    // save config
+    this._config = config;
+  }
+
+
+  /**
+   * Gets Census data service config.
+   */
+  get config() {
+    return this._config;
+  }
+
+
+  /**
+   * Gets US population stats.
+   * 
+   * @param location US location: state, county, zip or USA (default).
+   */
+  getPopulation(location) {
+    console.log(`Census:getPopulation(): location=${location}`);
+    // TODO
+  }
+
+} // end of Census class
+
+//export {Census as default}
+// use old school for jest.js
+exports["default"] = Census;
+module.exports = exports["default"];
