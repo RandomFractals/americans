68bde0ca4580bc9c6c17e181d183e22194d871fe
Taras Novak <taras.novak@randomfractals.com>
2016-12-25 11:34:20 -0600

    added County data model class + test for county query validation and census data lookups

---
+++ src/census/county.js
@@ -0,0 +1,60 @@
+'use strict';
+
+/**
+ * Defines US county with 5 digit FIPS code,
+ * county name and state for county census data lookups.
+ */
+class County {
+
+  /**
+  * Creates new county instance.
+  *
+  * @param code 5 digit FIPS code.
+  * @param name County name.
+  * @param state County state code.
+  *
+  * @see https://en.wikipedia.org/wiki/FIPS_county_code
+  */
+  constructor(code, name, state) {
+    // save county info
+    this._code = code;
+    this._name = name;
+    this._state = state;
+  }
+
+
+  /**
+   * Gets county code.
+   */
+  get code() {
+    return this._code;
+  }
+
+
+  /**
+   * Gets county name.
+   */
+  get name() {
+    return this._name;    
+  }
+
+
+  /**
+   * Gets county state abbreviation.
+   */
+  get state() {
+    return this._state;
+  }
+
+
+  /**
+   * @return Returns county name and state abbreviation.
+   */
+  toString() {
+    return `${this.name}, ${this.state}`;
+  }
+
+} 
+
+exports["default"] = County;
+module.exports = exports["default"];
---
+++ tests/census/county.test.js
@@ -0,0 +1,32 @@
+jest.dontMock('../../src/census/county.js');
+
+// import county class
+const County = require('../../src/census/county.js');
+
+// create county test data
+const county = {
+  code: 17031,
+  name: 'Cook County',
+  state: 'IL'
+};
+
+// create test county instance
+const testCounty = new County(county.code, county.name, county.state);
+
+// test county data model interface
+describe('County Data Model Tests', () => {
+
+  it('county code set/get test', () => {
+    expect(testCounty.code).toEqual(county.code);
+  });
+
+  it('county name set/get test', () => {
+    expect(testCounty.name).toEqual(county.name);
+  });
+
+  it('county state set/get test', () => {
+    expect(testCounty.state).toEqual(county.state);
+  });
+  
+});
+
