59225fdfc6c55d2911b9c86b3638d9d3a4024f68
Taras Novak <taras.novak@randomfractals.com>
2017-01-01 08:04:54 -0600

    added place.js data model class for loading US places config

---
+++ src/census/place.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const Region = require('./region.js');
+
+/**
+ * Defines US city, town, or village place with 5 digit FIPS code,
+ * state and county for place census data lookups.
+ * 
+ * @see https://www.census.gov/geo/reference/codes/place.html
+ */
+class Place extends Region {
+
+  /**
+  * Creates new Place class instance.
+  *
+  * @param code Place 5 digit FIPS code.
+  * @param name Place name.
+  * @param state Place state code.
+  * @param county Place county name.
+  *
+  * @see https://www.census.gov/geo/reference/codes/place.html
+  */
+  constructor(code, name, state, county) {
+    super(code, name);
+    this.state = state;
+    this.county = county;
+    this.type = 'place';
+
+    // create place key for lookups: lowercase and
+    // strip out ' city', ' town', ' village', and ' CDP'
+    this.key = this.name.replace(' city', '')
+      .replace(' town', '')
+      .replace(' village', '')
+      .replace(' CDP', '') // CDP - Census Designated Place
+      .toLowerCase()
+      .split(' ').join('');
+  }
+
+
+  /**
+   * @return Returns place name with state abbreviation.
+   */
+  toString() {
+    return `${this.name}, ${this.state}`;
+  }
+
+} 
+
+exports["default"] = Place;
+module.exports = exports["default"];
