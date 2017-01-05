e9b5e1abf61e7eaac214951f5eb8888630ab5565
Taras Novak <taras.novak@randomfractals.com>
2017-01-05 10:32:52 -0600

    get slack webhook url from .env and export it to slack.js chat client

--- src/utils/app-config.js
+++ src/utils/app-config.js
@@ -18,6 +18,12 @@ if (!WIT_TOKEN) {
   throw new Error('Missing WIT_TOKEN');
 }
 
+// get incoming Slack webhook url
+const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
+if (!SLACK_WEBHOOK_URL) {
+  throw new Error('Missing SLACK_WEBHOOK_URL');
+}
+
 // get FB page token and app secret
 const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
 const FB_APP_SECRET = process.env.FB_APP_SECRET;
@@ -50,8 +56,9 @@ if (!CENSUS_DATA_API_KEY) {
 module.exports = {
   BOT_AI: BOT_AI,
   WIT_TOKEN: WIT_TOKEN,
+  CENSUS_DATA_API_KEY: CENSUS_DATA_API_KEY,
+  SLACK_WEBHOOK_URL: SLACK_WEBHOOK_URL,
   FB_PAGE_TOKEN: FB_PAGE_TOKEN,
   FB_VERIFY_TOKEN: FB_VERIFY_TOKEN,
-  FB_APP_SECRET: FB_APP_SECRET,
-  CENSUS_DATA_API_KEY: CENSUS_DATA_API_KEY
+  FB_APP_SECRET: FB_APP_SECRET
 };
