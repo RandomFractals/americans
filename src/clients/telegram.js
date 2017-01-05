20609610b325768d4b35aa10687815f7691ac443
Taras Novak <taras.novak@randomfractals.com>
2017-01-05 16:43:36 -0600

    added telegram.js + tests clone setup, tbc...

---
+++ src/clients/telegram.js
@@ -0,0 +1,57 @@
+'use strict';
+
+// import bot client base class
+const BotClient = require('./bot-client.js');
+
+/**
+ * Defines Telegram class for Telegram bot chats.
+ * 
+ * @see https://core.telegram.org/bots/api
+ */
+class Telegram extends BotClient {
+
+  /**
+  * Creates new Telegram bot client instance for Telegram ping chats.
+  *
+  * @param config Telegram bot config.
+  */
+  constructor(config) {
+    super(config, 'Telegram'); // chat client name
+  }
+
+
+  /**
+   * Sends Telegram message response.
+   * 
+   * @param recipientId Telegram recipient channel or user id.
+   * @param messageText Message text to send.
+   */
+  sendMessage(recipientId, messageText) {
+    // create post message json data
+    let messageData = JSON.stringify({
+      channel: recipientId,
+      username: 'Americans',
+      icon_emoji: ':us:',
+      text: messageText
+    });
+
+    // send message via incoming Slack webhook url endpoint
+    console.log(`Slack.sendMessage(): request:${messageData}`);
+    const webHookUrl = this.config.SLACK_WEBHOOK_URL;
+    return fetch(webHookUrl, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: messageData,
+    })
+    .then(messageResponse => {
+      console.log(`Slack.sendMessage(): response:${messageResponse}`);
+      return messageResponse;
+    });
+  } // end of sendMessage()
+
+}
+
+//export {Telegram as default}
+// use old school for jest.js
+exports["default"] = Telegram;
+module.exports = exports["default"];
---
+++ tests/clients/telegram.test.js
@@ -0,0 +1,66 @@
+// load config
+const config = require('../../src/utils/app-config.js');
+
+// create Slack chat client interface instance
+const Telegram = require('../../src/clients/telegram.js');
+const telegram = new Telegram(config);
+const testUserId = '#americans';
+
+describe('Telegram Interface Tests', () => {
+
+  it('processMessage() exists', () => {
+    expect(telegram.processMessage).not.toBeNull();
+  });
+
+  it('sendMessage() exists', () => {
+    expect(telegram.sendMessage).not.toBeNull();
+  });
+});
+
+describe('Telegram processMessage() Tests', () => {
+
+  it('fails on missing message text', () => {
+    expect( () => {
+      telegram.processMessage({
+        sender: {}, recipient: {}, message: {}
+      });
+    }).toThrowError('Missing message text.');
+  });
+
+  xit('can process tests/message.json', () => {
+    // load test message
+    let messageData = null;    
+    require('fs').readFile('./message.json', 'utf8', function(err, data) {      
+      if (err) throw err; // ignore this error handling for now
+      messageData = JSON.parse(data);
+
+      // process test message
+      return telegram.processMessage(messageData.entry[0].messaging[0])
+        .then( (response) => {
+          expect(response).toBe(undefined); // no return, so undefined 
+        });
+    });
+  });
+
+  xit('can send a "test message"', () => {
+    const testMessage = 'test message';
+    return telegram.sendMessage(testUserId, testMessage)
+      .then( (response) => {
+        console.log(`send test message response: ${JSON.stringify(response)}`);
+        expect(response.message).toEqual(testMessage); 
+      });
+  });    
+
+  it('usa pop?', () => {
+    return telegram.processMessage({
+      sender: {id: testUserId}, 
+      recipient: {id: testUserId}, 
+      message: {text: 'usa pop?'}
+    })
+    .then( (response) => {
+      console.log(`population test response:  ${JSON.stringify(response)}`);
+      expect(response).not.toBeNull(); 
+    });
+  });    
+
+});
